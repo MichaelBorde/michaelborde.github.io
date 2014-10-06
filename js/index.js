20919385016db1aef4e4b60e8f4ef50b25112252
Michael Borde <michael.borde@gmail.com>
2014-10-06 23:56:22 +0200

    Les tags sont désormais mélangés

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
 $(document).ready(function () {
+  $('div.tags').melange('button');
 });
+
+(function ($) {
+  $.fn.melange = function (elementAMelanger) {
+    return this.each(function () {
+      var that = $(this);
+      var elementsAMelanger = that.children(elementAMelanger);
+
+      melangeLesElements();
+      supprimeLesElements();
+      ajouteLesElementsDansLeNouvelOrdre();
+
+      function melangeLesElements() {
+        elementsAMelanger.sort(function () {
+          return (Math.round(Math.random()) - 0.5);
+        });
+      }
+
+      function supprimeLesElements() {
+        that.remove(elementAMelanger);
+      }
+
+      function ajouteLesElementsDansLeNouvelOrdre() {
+        for (var i = 0; i < elementsAMelanger.length; i++) {
+          that.append(elementsAMelanger[i]);
+        }
+      }
+    });
+  }
+})(jQuery);
